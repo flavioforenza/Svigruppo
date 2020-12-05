b5f84712eb6964a2cca6c82f114e513173f8acba
Flavio Forenza <flavio.forenza@studenti.unimi.it>
2020-12-05 18:31:32 +0100

    Fix solution Exercise5

--- Exercise5/solution.sh
+++ Exercise5/solution.sh
@@ -6,32 +6,37 @@ mkdir Exercise5
 cd Exercise5
 
 git init
-printf 'pluto\n'>X
-printf 'pippo\n'> Z/Y
-cp Z/Y .
+
+printf "pluto\n" > X
+printf "pippo\n" > Y
+mkdir Z
+cp Y Z
 git add .
 git commit -m primo
 
-rm X
-mv Y X
-printf 'pippo e minnie\n'>D
-git add .
-git commit -m successivo
+printf "pippo e minnie\n" > X
+git add X
+git mv X D
+git mv Y X
+
+git commit -m successivo 
 
 git tag delivered
 
-mkdir tmp
-mv Z tmp
-mv tmp Z
-mv D Z/
-mv X Z/
-git add .
-git commit -m 'ultimo?'
+mkdir _Z
+git mv D X Z _Z
+git mv _Z Z
+git commit -m "ultimo?"
 
 printf 'sbagliato\n' > tmp
-
+git add tmp
+git rm -f --cached tmp Z/X  
 git checkout -b student
 #Working Dir and Index don't change
 git reset --soft delivered
 
-git rm --cached Z/X
+
+
+
+
+
